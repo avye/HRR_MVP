f3fed72c96f14a73871701bd7d60cfeb2ef0938e
Andrew Vye <a.vye26@gmail.com>
2016-05-23 13:49:32 -0700

    Created App component

---
+++ Client/src/components/App.jsx
@@ -0,0 +1,15 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <div>
+        <h2>Name that animal</h2>
+      </div>  
+    );
+  }
+}
+
+window.App = App;
